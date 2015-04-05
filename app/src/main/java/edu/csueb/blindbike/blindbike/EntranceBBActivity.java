587fbcc00426b72ebf1cb0880dc1aaed5f49a6ed
William <woverell@horizon.csueastbay.edu>
2015-04-05 14:31:46 -0700

    Added Portrait and Landscape UI for the Directions and Navigation Activities

--- app/src/main/java/edu/csueb/blindbike/blindbike/EntranceBBActivity.java
+++ app/src/main/java/edu/csueb/blindbike/blindbike/EntranceBBActivity.java
@@ -11,7 +11,7 @@ public class EntranceBBActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_entrance_bb);
+        setContentView(R.layout.activity_entrance_bb_landscape);
     }
 
 
